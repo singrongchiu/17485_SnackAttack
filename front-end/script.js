9f6c8ffbe0a8b253d20f5a16b57fe21e049f52b2
Yuqi Pan <yuqipan@utexas.edu>
2023-03-02 16:16:03 -0600

    clean up

--- front-end/script.js
+++ front-end/script.js
@@ -1,26 +1,11 @@
 // bound to button onclick
-function changeWord() {
+function submitUserPassword() {
     // get the value in the input element
-    let userInput = document.getElementById('adjective-input').value
+    let Username = document.getElementById('username').value
+
+    let Password = document.getElementById('password').value
     
     // change the inner HTML of the adjective (span) element
-    document.getElementById('adjective').innerHTML = userInput
-}
-
-// bound to input field oninput
-function toggleWordColor() {
-    // get the adjective (span) element
-    let adjective = document.getElementById('adjective')
-    
-    // see if the span element is in the .salmon class
-    let hasSalmon = adjective.className === 'salmon'
-
-    // if it is, remove the .salmon class
-    if (hasSalmon) {
-        adjective.className = ''
-    }
-    // otherwise, add the .salmon class
-    else {
-        adjective.className = 'salmon'
-    }
+    document.getElementById('username').innerHTML = Username
+    document.getElementById('password').innerHTML = Password
 }
